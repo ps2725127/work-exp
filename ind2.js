eedbada28cac50c51e31f270fbddcb8d91701904
Priyanshu Singh <ps2725127@gmail.com>
2024-06-13 22:36:35 +0530

    figlet work

--- ind2.js
+++ ind2.js
@@ -1,1 +1,9 @@
-let x=2 ;
+const fig = require("figlet");
+fig("Priyanshu singh", function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  });
